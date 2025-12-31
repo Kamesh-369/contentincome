9f7cc4745b8814711fedf652dd7c5228a5c47527
Kamesh-369 <kameshraghul.300@gmail.com>
2025-12-31 21:12:16 +0530

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
 
+import Script from "next/script";
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -21,6 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-L1QHWL912D"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L1QHWL912D');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} font-sans antialiased min-h-screen flex flex-col`}
       >
